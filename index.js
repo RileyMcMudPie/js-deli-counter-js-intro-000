6868267d74c028fd7f1080b7adf3854cea3f53d1
Riley McMudPie <rydonamusic@gmail.com>
2020-07-16 23:42:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ if(line.length > 0){
 }
 
 function currentLine(line){
+  var i;
+  for (i = 0; i < line.length; i++)
   if (line.length === 0){
     return 'The line is currently empty.'
   }else if (line.length > 0){
