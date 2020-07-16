023295bc287084f80feded11a42b2c33b872f40c
Riley McMudPie <rydonamusic@gmail.com>
2020-07-16 23:46:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if(line.length > 0){
 function currentLine(line){
   var i;
   var NumbersAndNames = [];
-  
+
   for (i = 0; i < line.length; i++){
     if (line.length === 0){
       return 'The line is currently empty.'
